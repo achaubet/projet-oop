4b05767ad71608ab660c694e8137f78c3c28fcf7
Chaubet Arnaud <a.chaubet@etud.univ-pau.fr>
2021-03-30 14:27:16 +0000

    Update projet-arnaud-chaubet-tristan-taupiac/ssp.cpp

--- projet-arnaud-chaubet-tristan-taupiac/ssp.cpp
+++ projet-arnaud-chaubet-tristan-taupiac/ssp.cpp
@@ -1,1 +1,19 @@
+#include "streaming-service.h" // for streaming_service_t
+#include "media.h" // for media_t
+#include "anime.h" // for anime_t
+#include "film.h" // for film_t
+#include "series.h" // for series_t
+#include <iostream> // for cout
+#include <pugixml.hpp> // for xml_document, xml_node, xml_attributes, xml_parse_result
+using namespace std;
+using namespace pugi;
+
+int main(int argc, char const *argv[]) {
+
+  anime_t anime;
+  anime.qualities_push_back(medium);
+  anime.qualities_push_back(high);
+  cout << anime;
+  return 0;
+}
 
