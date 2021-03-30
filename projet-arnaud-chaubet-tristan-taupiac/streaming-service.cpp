4d68808ec1aa668016196b3a977ea7aa96632b41
Chaubet Arnaud <a.chaubet@etud.univ-pau.fr>
2021-03-30 18:04:42 +0200

    handle_m et handle_mn

--- projet-arnaud-chaubet-tristan-taupiac/streaming-service.cpp
+++ projet-arnaud-chaubet-tristan-taupiac/streaming-service.cpp
@@ -17,3 +17,17 @@ string streaming_service_t::get_web() const{
 void streaming_service_t::handle_i() const{
   cout << this;
 }
+
+void streaming_service_t::handle_m() const{
+  int i = 0;
+  for(; i < static_cast<int>(this->medias.size()); i++){
+    this->medias[i].handle_m();
+  }
+}
+
+void streaming_service_t::handle_mn(string name) const{
+  int i = 0;
+  for(; i < static_cast<int>(this->medias.size()); i++){
+    this->medias[i].handle_mn(name);
+  }
+}
