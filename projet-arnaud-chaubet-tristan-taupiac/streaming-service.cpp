0c675316a4bcdb2ee180c6ce15c58c23b567c993
Chaubet Arnaud <a.chaubet@etud.univ-pau.fr>
2021-03-30 17:09:59 +0200

    Ajout handle_i

--- projet-arnaud-chaubet-tristan-taupiac/streaming-service.cpp
+++ projet-arnaud-chaubet-tristan-taupiac/streaming-service.cpp
@@ -1,4 +1,5 @@
 #include "streaming-service.h" // for streaming_service_h
+#include <iostream>
 
 streaming_service_t::streaming_service_t(){
   this->name = "undefined";
@@ -11,4 +12,8 @@ string streaming_service_t::get_name() const{
 
 string streaming_service_t::get_web() const{
   return this->web;
+}
+
+void streaming_service_t::handle_i() const{
+  cout << this;
 }
