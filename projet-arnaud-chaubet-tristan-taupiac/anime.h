4529be167b4dcc1a4965482e17903edc70d0d530
Chaubet Arnaud <a.chaubet@etud.univ-pau.fr>
2021-03-30 14:04:56 +0000

    Update anime.h

--- projet-arnaud-chaubet-tristan-taupiac/anime.h
+++ projet-arnaud-chaubet-tristan-taupiac/anime.h
@@ -1,1 +1,19 @@
-
+/**
+ * \file anime.h
+ */
+#ifndef ANIME_H
+#define ANIME_H
+#include "media.h" // for media_t
+/**
+ * An anime.
+ */
+class anime_t: public media_t
+{
+          public:
+                  /**
+                   * Constructs a new anime.
+                   * Initializes its announcement to "The anime is played!".
+                   */
+                 anime_t();
+};
+#endif
