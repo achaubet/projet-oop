f82345a8f55e0ae7ddd7e5b0ba9f8c91a7cd67df
Chaubet Arnaud <a.chaubet@etud.univ-pau.fr>
2021-03-31 20:31:28 +0200

    Ajout handle_my

--- projet-arnaud-chaubet-tristan-taupiac/streaming-service.cpp
+++ projet-arnaud-chaubet-tristan-taupiac/streaming-service.cpp
@@ -1,5 +1,5 @@
 #include "streaming-service.h" // for streaming_service_h
-#include <iostream>
+#include <iostream> // for cout
 
 streaming_service_t::streaming_service_t(){
   this->name = "undefined";
@@ -29,5 +29,12 @@ void streaming_service_t::handle_mn(string name) const{
   int i = 0;
   for(; i < static_cast<int>(this->medias.size()); i++){
     this->medias[i].handle_mn(name);
+  }
+}
+
+void streaming_service_t::handle_my(int year) const{
+  int i = 0;
+  for(; i < static_cast<int>(this->medias.size()); i++){
+    this->medias[i].handle_my(year);
   }
 }
